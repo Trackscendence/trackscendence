7d06b231e9ee813f0bc9cc381634c397fe98cfbe
Skyy Moore <124527392+mooreApps22@users.noreply.github.com>
2026-02-08 21:11:04 -0800

    Add initial app.js to display 'Hello, World!'

---
+++ srcs/requirements/node/public/app.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const  root = document.createElement("div");
+  root.textContent = "Hello, World!";
+  root.style.fontSize = "2rem";
+  root.style.marginTop = "2rem";
+  document.body.appendChild(root);
+});

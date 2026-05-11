5461bb15d03d5956788fa2689aae3ce290fefb3a
shenghongzhong <howdavidlearncoding@gmail.com>
2026-05-11 23:54:43 +0800

    Centralize API v1 route prefix

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,3 @@
-const { Router } = require('express')
 const v1Router = require('#routes/v1')
 
-const router = Router()
-
-router.use('/v1', v1Router)
-
-module.exports = router
+module.exports = v1Router

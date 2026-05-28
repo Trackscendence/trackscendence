434a14ed2e70ac7f4fc3bc04b3f74f4202a3f27b
Sergi Rodrigo <srdlr@gmail.com>
2026-05-28 13:57:44 +0100

    prepare environment
    client:
    - install socket.io-client
    - change npm dev script to add websocket url environment var for development mode
    
    server:
    - install socket.io

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_PROXY_TARGET || 'http://localhost:3001',
+        target: process.env.VITE_API_PROXY_TARGET || 'http://server:3001',
         changeOrigin: true,
       },
     },

94fee52cde19cbfe8e1a5e09ad7125307b550e39
Sergi Rodrigo <srdlr@gmail.com>
2026-05-31 11:52:11 +0100

    add websocket servoce to the server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,25 @@
 const app = require('#app')
+const { createServer } = require('node:http')
 const config = require('#utils/config')
 const logger = require('#utils/logger')
 
+const server = createServer(app)
 
-app.listen(config.PORT, () => {
+const { Server } = require('socket.io')
+
+const io = new Server(server, {
+  path: '/websocket/'
+})
+
+io.on('connection', (socket) => {
+	console.log('user connected:', socket.id)
+		
+	socket.on('disconnect', () => {
+			console.log('user disconnected:', socket.id)
+	})
+
+})	
+
+server.listen(config.PORT, () => {
 	logger.info(`Server running on port ${config.PORT}`)
 })

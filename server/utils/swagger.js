5221d523e76d8e862bf951f8f56ce5a6c1bfe3db
Skyy Moore <mooreapps2022@gmail.com>
2026-06-22 23:25:29 -0700

    feat(auth): added security schemes definition for bearer authentication

--- server/utils/swagger.js
+++ server/utils/swagger.js
@@ -13,6 +13,15 @@ const options = {
         url: '/api/v1',
       },
     ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
   },
   apis: ['./src/modules/**/*.routes.js'],
 }

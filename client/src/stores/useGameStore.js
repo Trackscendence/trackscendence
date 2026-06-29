1013c70090f9972c719b64deb06ffb973286f0a4
Dmytro Melnyk <179873458+demm05@users.noreply.github.com>
2026-06-29 22:43:37 +0100

    feat(frontend): prep useGameStore and useSocketStore for PR 144/145 integration

--- client/src/stores/useGameStore.js
+++ client/src/stores/useGameStore.js
@@ -1,13 +1,28 @@
 import { create } from 'zustand'
+import { socket } from '@/services/socket'
 
 const useGameStore = create((set) => ({
   matchHistory: [],
   leaderboard: [],
   currentMatch: null,
+  gameState: null,
+  lobbyCount: 0,
+  gameError: null,
 
   setMatchHistory: (matchHistory) => set({ matchHistory }),
   setLeaderboard: (leaderboard) => set({ leaderboard }),
   setCurrentMatch: (currentMatch) => set({ currentMatch }),
+
+  setGameState: (gameState) => set({ gameState }),
+  setLobbyCount: (lobbyCount) => set({ lobbyCount }),
+  setGameError: (gameError) => set({ gameError }),
+  clearGame: () => set({ gameState: null, gameError: null }),
+
+  joinLobby: () => socket.emit('join_lobby'),
+  playCard: (gameId, cardIndex, declaredColor) =>
+    socket.emit('game:play_card', { gameId, cardIndex, declaredColor }),
+  drawCard: (gameId) => socket.emit('game:draw_card', { gameId }),
+  passTurn: (gameId) => socket.emit('game:pass_turn', { gameId }),
 }))
 
 export default useGameStore
